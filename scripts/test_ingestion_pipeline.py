070624d6ea47d42561960268431440c6da3b5409
Sumanth Kumar Gaddam <sumanthkumar.gaddam@cashe.co.in>
2026-06-18 15:48:52 +0530

    add requirements.txt and update test_ingestion_pipeline

--- scripts/test_ingestion_pipeline.py
+++ scripts/test_ingestion_pipeline.py
@@ -1,3 +1,47 @@
+# Glue import the secrets from the secret manager and here is template
+
+# import os
+# import json
+# import boto3
+
+# client = boto3.client("secretsmanager")
+
+# secret = client.get_secret_value(
+#     SecretId="mf-ingestion-config"
+# )
+
+# config = json.loads(secret["SecretString"])
+
+# for key, value in config.items():
+#     os.environ[key] = str(value)
+
+# #
+# # ONLY NOW import application code
+# #
+# from ingestion.bootstrap.container import IngestionContainer
+# from app.core.database import SessionLocal
+
+
+# from awsglue.utils import getResolvedOptions
+# import sys
+
+# args = getResolvedOptions(
+#     sys.argv,
+#     ["bucket", "key"]
+# )
+
+# bucket = args["bucket"]
+# key = args["key"]
+
+# print(bucket)
+# print(key)
+
+# pipeline.run_from_s3(
+#     bucket=bucket,
+#     key=key
+# )
+
+
 from nt import pipe
 from ingestion.bootstrap.ingestion_container import IngestionContainer
 from app.core.database import SessionLocal
