5845318e03efd878b572f9cbba4283d7cbec9d7d
Sumanth Kumar Gaddam <sumanthkumar.gaddam@cashe.co.in>
2026-06-19 14:42:37 +0530

    make ecs as sync

--- ecs_entrypoint.py
+++ ecs_entrypoint.py
@@ -4,12 +4,12 @@ import asyncio
 from ingestion.bootstrap.ingestion_container import IngestionContainer
 from app.core.database import SessionLocal
 
-async def main():
+def main():
     bucket = os.environ['BUCKET']
     key = os.environ['KEY']
     document_type = os.environ['DOCUMENT_TYPE']
     try:
-        async with SessionLocal() as db:
+        with SessionLocal() as db:
             pipeline = IngestionContainer.build(db)
             result = pipeline.run_from_s3(bucket=bucket, key=key, document_type=document_type)
             print(result)
@@ -18,5 +18,5 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
 
