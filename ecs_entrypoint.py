df7221ee3c82ee1ba3be585e722a2c16417e1ca7
Sumanth Kumar Gaddam <sumanthkumar.gaddam@cashe.co.in>
2026-06-19 16:07:09 +0530

    ecs entry point revert

--- ecs_entrypoint.py
+++ ecs_entrypoint.py
@@ -3,33 +3,8 @@ import asyncio
 
 from ingestion.bootstrap.ingestion_container import IngestionContainer
 from app.core.database import SessionLocal
-from app.core.config import settings
-import psycopg
 
 def main():
-    print("HOST:", settings.POSTGRES_HOST)
-    print("PORT:", settings.POSTGRES_PORT)
-    print("DB:", settings.POSTGRES_DB)
-    print("USER:", settings.POSTGRES_USER)
-
-    try:
-        conn = psycopg.connect(
-            host=settings.POSTGRES_HOST,
-            port=settings.POSTGRES_PORT,
-            user=settings.POSTGRES_USER,
-            password=settings.POSTGRES_PASSWORD,
-            dbname=settings.POSTGRES_DB,
-            connect_timeout=10,
-            sslmode="require"  # try this first
-        )
-
-        print("POSTGRES CONNECTION SUCCESS")
-        conn.close()
-
-    except Exception as e:
-        print("POSTGRES CONNECTION FAILED")
-        print(repr(e))
-        raise
     bucket = os.environ['BUCKET']
     key = os.environ['KEY']
     document_type = os.environ['DOCUMENT_TYPE']
